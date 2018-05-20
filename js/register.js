b3691930692341298c8f4da570a87674844cb8ca
Bekir Sefa BAL <sefabal@outlook.com>
2018-05-20 21:25:16 +0300

    Add files via upload
    New checks added.

--- js/register.js
+++ js/register.js
@@ -3,6 +3,8 @@ function checkForm() {
 	var nameVal = name.value;
 	var surname = document.getElementById("surname");
 	var surNameVal = surname.value;
+	var email = document.getElementById("email");
+	var emailVal = email.value;
 
 	var password = document.getElementById("password");
 	var passwordValue = password.value;
@@ -15,14 +17,40 @@ function checkForm() {
 	var patt1 = /[w_]/g;
 
 	
+	if(nameVal.length == 0 ) {
+		alert("Name can't be empty.");
+		return false;
+	}
 
+	if(surNameVal.length == 0) {
+		alert("Surname can't be empty.");
+		return false;
+	}
+
+	if(emailVal.length == 0) {
+		
+		alert("Email can't be empty");
+		return false;
+	}
+
+
+	if(passwordValue.length == 0) {
+		alert("Password can't be empty");
+		return false;
+	}
+
+	
 	if(nameVal.match(patt1) != null || surNameVal.match(patt1) != null) {
 		console.log("Check the inputs");
 		alert("Name and Surname must be a character.")
 		return false;
 	}
 
-	
+	if(passwordValue.length < 6 || passwordValue.length > 14) {
+		alert("Your password should be longer 6 and lower than 14");
+		return false;
+	}
+
 	if(checkPassword(passwordValue,uppercase) == false) {
 		return false;
 	}
@@ -38,12 +66,6 @@ function checkForm() {
 	if(checkPassword(passwordValue,numeric) == false) {
 		return false;
 	}
-	
-	if(passwordValue.length < 6 || passwordValue.length > 14) {
-		alert("Your password should be longer 6 and lower than 14");
-		return false;
-	}
-
 
 }
 
